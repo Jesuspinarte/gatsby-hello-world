dddcdf13588a7d0aeed8cfde9b1db14550d3b4c6
Jesus Pineda <jesus.pineda@rappi.com>
2020-11-21 18:21:05 -0500

    [TUTORIAL-5] - Gatsby tutoriqal 5 made

---
+++ src/pages/my-file.js
@@ -0,0 +1,53 @@
+// External imports
+import React from 'react';
+import { graphql } from 'gatsby';
+
+// Intertal imports
+// -- HOC
+import Layout from '../components/_hoc/layout/Layout';
+
+export default ({ data }) => {
+  console.log(data);
+  return (
+    <Layout>
+      <div>
+        <h1>My Site's Files</h1>
+        <table>
+          <thead>
+            <tr>
+              <th>relativePath</th>
+              <th>prettySize</th>
+              <th>extension</th>
+              <th>birthTime</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.allFile.edges.map(({ node }, index) => (
+              <tr key={index}>
+                <td>{node.relativePath}</td>
+                <td>{node.prettySize}</td>
+                <td>{node.extension}</td>
+                <td>{node.birthTime}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </Layout>
+  );
+};
+
+export const myFileQuery = graphql`
+  query {
+    allFile {
+      edges {
+        node {
+          relativePath
+          prettySize
+          extension
+          birthTime(fromNow: true)
+        }
+      }
+    }
+  }
+`;
