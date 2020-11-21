5628ccd7891c4d402cf9490a3cd1e7f949dd2361
Jesus Pineda <jesus.pineda@rappi.com>
2020-11-21 18:22:54 -0500

    [TUTORIAL-5] - Gatsby tutoriqal 5 made

--- src/pages/my-file.js
+++ src/pages/my-file.js
@@ -6,36 +6,33 @@ import { graphql } from 'gatsby';
 // -- HOC
 import Layout from '../components/_hoc/layout/Layout';
 
-export default ({ data }) => {
-  console.log(data);
-  return (
-    <Layout>
-      <div>
-        <h1>My Site's Files</h1>
-        <table>
-          <thead>
-            <tr>
-              <th>relativePath</th>
-              <th>prettySize</th>
-              <th>extension</th>
-              <th>birthTime</th>
+export default ({ data }) => (
+  <Layout>
+    <div>
+      <h1>My Site's Files</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>relativePath</th>
+            <th>prettySize</th>
+            <th>extension</th>
+            <th>birthTime</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.allFile.edges.map(({ node }, index) => (
+            <tr key={index}>
+              <td>{node.relativePath}</td>
+              <td>{node.prettySize}</td>
+              <td>{node.extension}</td>
+              <td>{node.birthTime}</td>
             </tr>
-          </thead>
-          <tbody>
-            {data.allFile.edges.map(({ node }, index) => (
-              <tr key={index}>
-                <td>{node.relativePath}</td>
-                <td>{node.prettySize}</td>
-                <td>{node.extension}</td>
-                <td>{node.birthTime}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-    </Layout>
-  );
-};
+          ))}
+        </tbody>
+      </table>
+    </div>
+  </Layout>
+);
 
 export const myFileQuery = graphql`
   query {
