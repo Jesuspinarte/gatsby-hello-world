a53b7010fff3e42b59f11dad65d13ab26355fb22
Jesus Pineda <jesus.pineda@rappi.com>
2020-11-21 18:38:10 -0500

    [TUTORIAL-6] - Transformer plugins

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,45 @@
 // External imports
+import { graphql } from 'gatsby';
 import React from 'react';
 
 // Internal imports
 // -- HOC
 import Layout from '../components/_hoc/layout/Layout';
 
-export default () => (
+export default ({ data }) => (
   <Layout>
-    <h1>Amazing Pandas Eating Things</h1>
     <div>
-      <img
-        src="https://2.bp.blogspot.com/-BMP2l6Hwvp4/TiAxeGx4CTI/AAAAAAAAD_M/XlC_mY3SoEw/s1600/panda-group-eating-bamboo.jpg"
-        alt="Group of pandas eating bamboo"
-      />
+      <h1>Amazing Pandas Eating Things</h1>
+      <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
+      {data.allMarkdownRemark.edges.map(({ node }) => (
+        <div key={node.id}>
+          <h3>
+            {node.frontmatter.title}{' '}
+            <span>
+              — {node.frontmatter.date}
+            </span>
+          </h3>
+          <p>{node.excerpt}</p>
+        </div>
+      ))}
     </div>
   </Layout>
 );
 
+export const query = graphql`
+  query {
+    allMarkdownRemark {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "DD MMMM, YYYY")
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`;
